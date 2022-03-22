06a7bc1340084abbf5f0ee62444ba61041d14f3a
gerarjon <gerarsuaverdez@gmail.com>
2022-03-21 20:41:24 -0700

    testing axios post url with different url

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -7,7 +7,7 @@ const SignUp = ({setIsAuth}) => {
   const [user, setUser] = useState(null);
 
   const signUp = () => {
-    Axios.post("/signup", user).then(res => {
+    Axios.post("tic-tac-toe/signup", user).then(res => {
       const { token, userId, firstName, lastName, username, hashedPassword } = res.data;
       cookies.set("token", token);
       cookies.set("firstName", firstName);
